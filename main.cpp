606c738204ba738ea1e5a7ad0c49c50fb0a11be6
paolo21d <paolo21d@gmail.com>
2018-03-10 12:20:43 +0100

    Stworzono interfejs użytkownika (UI)

--- main.cpp
+++ main.cpp
@@ -14,9 +14,55 @@ int main() {
     Complex zesp2(re, im);
     cout << zesp2;
     Complex w = zesp1 + zesp2;*/
-
-
-    cout << Complex(1, 1) + 5;
-    //cout<<"Modul: "<<zesp1.modul()<<endl;
+    int opcja;
+    double re, im;
+    while (1) {
+        cout << "MENU" << endl;
+        cout << "1 Dodaj liczby" << endl;
+        cout << "2 Odejmij liczby" << endl;
+        cout << "3 Pomnoz liczby" << endl;
+        cout << "4 Oblicz modul liczby" << endl;
+        cout << "5 Oblicz argument liczby" << endl;
+        cout << "0 Wyjscie z programu" << endl;
+        cout << "Liczby wprowadzamy w postaci: czesc_rzeczywista czesc_urojona" << endl;
+        cin >> opcja;
+        if (opcja == 1) {
+            cout << "Podaj liczbe z1 ";
+            cin >> re >> im;
+            Complex z1(re, im);
+            cout << "Podaj liczbe z2 ";
+            cin >> re >> im;
+            Complex z2(re, im);
+            cout << endl << "Wynik " << z1 << " + " << z2 << " = " << z1 + z2 << endl;
+        } else if (opcja == 2) {
+            cout << "Podaj liczbe z1 ";
+            cin >> re >> im;
+            Complex z1(re, im);
+            cout << "Podaj liczbe z2 ";
+            cin >> re >> im;
+            Complex z2(re, im);
+            cout << endl << "Wynik " << z1 << " - (" << z2 << ") = " << z1 - z2 << endl;
+        } else if (opcja == 3) {
+            cout << "Podaj liczbe z1 ";
+            cin >> re >> im;
+            Complex z1(re, im);
+            cout << "Podaj liczbe z2 ";
+            cin >> re >> im;
+            Complex z2(re, im);
+            cout << endl << "Wynik " << z1 << " * (" << z2 << ") = " << z1 * z2 << endl;
+        } else if (opcja == 4) {
+            cout << "Podaj liczbe z ";
+            cin >> re >> im;
+            Complex z(re, im);
+            cout << "|" << z << "| = " << z.modul() << endl;
+        } else if (opcja == 5) {
+            cout << "Podaj liczbe z ";
+            cin >> re >> im;
+            Complex z(re, im);
+            cout << "arg(" << z << ") = " << z.argument() << endl;
+        } else {
+            break;
+        }
+    }
     return 0;
 }
