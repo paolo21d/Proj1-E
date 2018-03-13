039a149760290398c541aa3868c347e39d7b19c1
paolo21d <paolo21d@gmail.com>
2018-03-13 20:24:25 +0100

    Walidacja danych po wczytywaniu strumieniem cin>>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
@@ -26,73 +27,118 @@ int main() {
         cout << "0 Wyjscie z programu" << endl;
         cout << "Liczby wprowadzamy w postaci: czesc_rzeczywista czesc_urojona" << endl;
         //cin >> opcja;
-        while (scanf("%d", &opcja) != 1) {
+        /*while (scanf("%d", &opcja) != 1) {
             while (getchar() != '\n');
             cout << endl << "Blad! Podaj odpowiedni numer ";
+        }*/
+        while (!(cin >> re >> im)) {
+            cout << endl << "Blad! Podaj odpowiedni numer ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
         if (opcja == 1) {
             cout << "Podaj liczbe z1 ";
             //cin >> re >> im;
-            while (scanf("%lf%lf", &re, &im) != 2) {
+            /*while (scanf("%lf%lf", &re, &im) != 2) {
                 while (getchar() != '\n');
                 cout << endl << "Blad! Podaj liczbe z1 ";
+            }*/
+            while (!(cin >> re >> im)) {
+                cout << endl << "Blad! Podaj liczbe z1 ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
             }
             Complex z1(re, im);
             cout << "Podaj liczbe z2 ";
             //cin >> re >> im;
-            while (scanf("%lf%lf", &re, &im) != 2) {
+            /*while (scanf("%lf%lf", &re, &im) != 2) {
                 while (getchar() != '\n');
                 cout << endl << "Blad! Podaj liczbe z2 ";
+            }*/
+            while (!(cin >> re >> im)) {
+                cout << endl << "Blad! Podaj liczbe z2 ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
             }
             Complex z2(re, im);
             cout << "Wynik " << z1 << " + " << z2 << " = " << z1 + z2 << endl;
         } else if (opcja == 2) {
             cout << "Podaj liczbe z1 ";
             //cin >> re >> im;
-            while (scanf("%lf%lf", &re, &im) != 2) {
+            /*while (scanf("%lf%lf", &re, &im) != 2) {
                 while (getchar() != '\n');
                 cout << endl << "Blad! Podaj liczbe z1 ";
+            }*/
+            while (!(cin >> re >> im)) {
+                cout << endl << "Blad! Podaj liczbe z1 ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
             }
             Complex z1(re, im);
             cout << "Podaj liczbe z2 ";
             //cin >> re >> im;
-            while (scanf("%lf%lf", &re, &im) != 2) {
+            /*while (scanf("%lf%lf", &re, &im) != 2) {
                 while (getchar() != '\n');
                 cout << endl << "Blad! Podaj liczbe z2 ";
+            }*/
+            while (!(cin >> re >> im)) {
+                cout << endl << "Blad! Podaj liczbe z2 ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
             }
             Complex z2(re, im);
             cout << "Wynik " << z1 << " - (" << z2 << ") = " << z1 - z2 << endl;
         } else if (opcja == 3) {
             cout << "Podaj liczbe z1 ";
             //cin >> re >> im;
-            while (scanf("%lf%lf", &re, &im) != 2) {
+            /*while (scanf("%lf%lf", &re, &im) != 2) {
                 while (getchar() != '\n');
                 cout << endl << "Blad! Podaj liczbe z1 ";
+            }*/
+            while (!(cin >> re >> im)) {
+                cout << endl << "Blad! Podaj liczbe z1 ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
             }
             Complex z1(re, im);
             cout << "Podaj liczbe z2 ";
             //cin >> re >> im;
-            while (scanf("%lf%lf", &re, &im) != 2) {
+            /*while (scanf("%lf%lf", &re, &im) != 2) {
                 while (getchar() != '\n');
                 cout << endl << "Blad! Podaj liczbe z2 ";
+            }*/
+            while (!(cin >> re >> im)) {
+                cout << endl << "Blad! Podaj liczbe z2 ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
             }
             Complex z2(re, im);
             cout << "Wynik " << z1 << " * (" << z2 << ") = " << z1 * z2 << endl;
         } else if (opcja == 4) {
             cout << "Podaj liczbe z ";
             //cin >> re >> im;
-            while (scanf("%lf%lf", &re, &im) != 2) {
+            /*while (scanf("%lf%lf", &re, &im) != 2) {
                 while (getchar() != '\n');
                 cout << endl << "Blad! Podaj liczbe z ";
+            }*/
+            while (!(cin >> re >> im)) {
+                cout << endl << "Blad! Podaj liczbe z ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
             }
             Complex z(re, im);
             cout << "|" << z << "| = " << z.modul() << endl;
         } else if (opcja == 5) {
             cout << "Podaj liczbe z ";
             //cin >> re >> im;
-            while (scanf("%lf%lf", &re, &im) != 2) {
+            /*while (scanf("%lf%lf", &re, &im) != 2) {
                 while (getchar() != '\n');
                 cout << endl << "Blad! Podaj liczbe z ";
+            }*/
+            while (!(cin >> re >> im)) {
+                cout << endl << "Blad! Podaj liczbe z1 ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
             }
             Complex z(re, im);
             cout << "arg(" << z << ") = " << z.argument() << endl;
